db443aefbf8bb59c11745e93c125dd27b062c198
UnicusCoder <mahiuddin.ahmed.neel@gmail.com>
2023-07-04 20:12:23 +0600

    Initial commit

---
+++ puddle/puddle/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from core.views import index, contact
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('admin/', admin.site.urls),
+]
+
